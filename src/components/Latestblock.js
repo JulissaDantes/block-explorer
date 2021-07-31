5b0bdd5ff8b3c167731402f9a7ef65ad55fac530
JulissaDantes <julissa@singularsound.com>
2021-07-31 16:05:37 -0400

    Pulling latest block data

--- src/components/Latestblock.js
+++ src/components/Latestblock.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect  }  from 'react'
+import  { Wallet, utils, providers } from 'ethers';
+const { JsonRpcProvider } = providers;
+
 
 export default function Latestblock() {
+    
+    const [number, setnumber] = useState(0);
+    const [hash, sethash] = useState(0);
+    const [parentHash, setparentHash] = useState(0);
+    const [nonce, setnonce] = useState(0);
+    const [transactionsRoot, settransactionsRoot] = useState(0);
+    const [stateRoot, setstateRoot] = useState(0);
+    const [receiptsRoot, sereceiptsRoot] = useState(0);
+    const [miner, setminer] = useState(0);
+    const [difficulty, setdifficulty] = useState(0);
+    const [totalDifficulty, settotalDifficulty] = useState(0);
+    const [gasLimit, setgasLimit] = useState(0);
+    const [gasUsed, setgasUsed] = useState(0);
+    const [timestamp, settimestamp] = useState(0);
+    const [transactions, settransactions] = useState(0);
+
+    useEffect(() => {
+        (async () => {
+          const provider = new JsonRpcProvider("https://rinkeby.infura.io/v3/433a74c66045425ba8fdf7f1cb23ffdb");
+          const latestBlockNum = await provider.getBlockNumber();
+          console.log('latest block number', latestBlockNum);
+          const block = await provider.getBlock(latestBlockNum,false);
+          setnumber(block.number);          
+          sethash(block.hash);
+          setparentHash(block.parentHash);          
+          setnonce(block.nonce);
+          settransactionsRoot(block.transactionsRoot);          
+          setstateRoot(block.stateRoot);
+          sereceiptsRoot(block.receiptsRoot);         
+          setminer(block.miner);
+          setdifficulty(block.difficulty);          
+          settotalDifficulty(block.totalDifficulty);
+          setgasLimit(utils.formatEther(block.gasLimit._hex));          
+          setgasUsed(utils.formatEther(block.gasUsed._hex));
+          settimestamp(block.timestamp);          
+          settransactions(block.transactions);
+        })()
+      }, [])
+
     return (
         <div>
-            Latest block
+             <p>Latest block:{number}</p>  
+             Hash: {hash}<br/>
+             ParentHash: {parentHash}<br/>
+             Nonce: {nonce}<br/>
+             TransactionsRoot: {transactionsRoot}<br/>
+             StateRoot: {stateRoot}<br/>
+             ReceiptsRoot: {receiptsRoot}<br/>
+             Miner: {miner}<br/>
+             Difficulty: {difficulty}<br/>
+             TotalDifficulty: {totalDifficulty}<br/>
+             GasLimit: {gasLimit}<br/>
+             GasUsed: {gasUsed}<br/>
+             Timestamp: {timestamp}<br/>
+             Transactions hashes: {transactions.map((tx) => <div>{tx}</div>)}
         </div>
     )
 }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -8,17 +8,7 @@ const { JsonRpcProvider } = providers;
 
 
 function Routes() {
-  /*getBlockChainData()
 
-  async function getBlockChainData(){
-    const provider = new JsonRpcProvider("https://rinkeby.infura.io/v3/433a74c66045425ba8fdf7f1cb23ffdb");
-
-    // create a new wallet with a simple private key
-    const privateKey = "0x91e666c2ee8463dbf3004730d389a8130c2ceba6e543ded0c80ea385ca72168f";
-    const wallet = new Wallet(privateKey, provider);
-    
-    wallet.getBalance().then((x) => console.log(utils.formatEther(x)));
-  }*/
   return (
     <BrowserRouter>
       <Switch>
